99e830a635e072212ff4987ddd8e9bd1ed141561
DreamerPlays <31022273+DreamerPlays@users.noreply.github.com>
2017-11-25 00:38:21 +0100

    Add files via upload
    Release/OriPatcher, then choose version and select the assembly csharp in your managed folder

---
+++ OriMultiplayerPatcher/OriMultiplayerPatcher/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace OriMultiplayerPatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ori = new System.Windows.Forms.Button();
+            this.Ori_de = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Ori
+            // 
+            this.Ori.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ori.Location = new System.Drawing.Point(12, 12);
+            this.Ori.Name = "Ori";
+            this.Ori.Size = new System.Drawing.Size(162, 32);
+            this.Ori.TabIndex = 0;
+            this.Ori.Text = "Ori";
+            this.Ori.UseVisualStyleBackColor = true;
+            this.Ori.Click += new System.EventHandler(this.Ori_Click);
+            // 
+            // Ori_de
+            // 
+            this.Ori_de.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ori_de.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Ori_de.Location = new System.Drawing.Point(12, 50);
+            this.Ori_de.Name = "Ori_de";
+            this.Ori_de.Size = new System.Drawing.Size(162, 32);
+            this.Ori_de.TabIndex = 1;
+            this.Ori_de.Text = "Ori DE";
+            this.Ori_de.UseVisualStyleBackColor = true;
+            this.Ori_de.Click += new System.EventHandler(this.Ori_de_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(186, 93);
+            this.Controls.Add(this.Ori_de);
+            this.Controls.Add(this.Ori);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Ori;
+        private System.Windows.Forms.Button Ori_de;
+    }
+}
+
---
+++ OriMultiplayerPatcher/OriMultiplayerPatcher/Form1.cs
@@ -0,0 +1,58 @@
+﻿using OriMultiplayerPatcher.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace OriMultiplayerPatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Ori_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog d1 = new OpenFileDialog();
+            d1.Filter = "Assembly-CSharp (*.dll)|Assembly-CSharp.dll";
+            if (d1.ShowDialog() == DialogResult.OK)
+            {
+                string Filename = d1.FileName;
+                if (File.Exists(Path.Combine(Path.GetDirectoryName(Filename), Path.GetFileNameWithoutExtension(Filename) + ".bak")))
+                {
+                    File.Delete(Path.Combine(Path.GetDirectoryName(Filename), Path.GetFileNameWithoutExtension(Filename) + ".bak"));
+                }
+                File.Copy(Filename, Path.Combine(Path.GetDirectoryName(Filename), Path.GetFileNameWithoutExtension(Filename) + ".bak"));
+                File.WriteAllBytes(Filename, Resources.Ori);
+                File.WriteAllBytes(Path.GetDirectoryName(Filename) + "OriMultiplayer.dll", Resources.OriMultiplayer);
+                MessageBox.Show("Patched...");
+            }
+        }
+
+        private void Ori_de_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog d1 = new OpenFileDialog();
+            d1.Filter = "Assembly-CSharp (*.dll)|Assembly-CSharp.dll";
+            if (d1.ShowDialog() == DialogResult.OK)
+            {
+                string Filename = d1.FileName;
+                if (File.Exists(Path.Combine(Path.GetDirectoryName(Filename), Path.GetFileNameWithoutExtension(Filename) + ".bak")))
+                {
+                    File.Delete(Path.Combine(Path.GetDirectoryName(Filename), Path.GetFileNameWithoutExtension(Filename) + ".bak"));
+                }
+                File.Copy(Filename, Path.Combine(Path.GetDirectoryName(Filename), Path.GetFileNameWithoutExtension(Filename) + ".bak"));
+                File.WriteAllBytes(Filename, Resources.OriDE);
+                File.WriteAllBytes(Path.GetDirectoryName(Filename) + "OriMultiplayer.dll", Resources.OriMultiplayer);
+                MessageBox.Show("Patched...");
+            }
+        }
+    }
+}
